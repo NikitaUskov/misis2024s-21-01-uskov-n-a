2dd5c6d18e20457e13c469fd1fa50613486ec161
NikitaUskov <uskovnikita2020@gmail.com>
2024-06-14 15:13:17 +0300

    up lab09

--- prj.lab/lab09/main.cpp
+++ prj.lab/lab09/main.cpp
@@ -1,127 +1,146 @@
 #include <opencv2/opencv.hpp>
+#include <string>
 #include <iostream>
 #include <vector>
-#include <cmath>
 
-struct Data {
-    cv::Point point;
-};
-
-std::vector<Data> readDataFromJson(const std::string& jsonFilePath) {
-    cv::FileStorage fs(jsonFilePath, cv::FileStorage::READ);
-    if (!fs.isOpened()) {
-        std::cerr << "Failed to open JSON file: " << jsonFilePath << std::endl;
-        return {};
+// Функция Gray World для выравнивания баланса белого
+cv::Mat grayWorld(const cv::Mat& src) {
+    if (src.empty()) {
+        std::cerr << "Error: Input image is empty!" << std::endl;
+        return cv::Mat();
     }
 
-    std::vector<Data> dataVector;
-
-    cv::FileNode objects = fs["objects"];
-    for (const auto& obj : objects) {
-        Data data;
-        data.point.x = static_cast<int>(obj["p"][0]);
-        data.point.y = static_cast<int>(obj["p"][1]);
-
-        dataVector.push_back(data);
-    }
-
-    return dataVector;
-}
-
-cv::Mat sRGB_to_linRGB(cv::Mat& image) {
-    cv::Mat lin_image(image.rows, image.cols, CV_32FC3);
-
-    for (int i = 0; i < image.rows; i++) {
-        for (int j = 0; j < image.cols; j++) {
-            cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
-            cv::Vec3f lin_pixel;
-
-            for (int k = 0; k < 3; k++) {
-                float c = pixel[k] / 255.0f;
-
-                if (c <= 0.04045) {
-                    lin_pixel[k] = c / 12.92f;
-                } else {
-                    lin_pixel[k] = pow((c + 0.055f) / 1.055f, 2.4f);
-                }
-            }
-
-            lin_image.at<cv::Vec3f>(i, j) = lin_pixel;
-        }
-    }
-
-    return lin_image;
-}
-
-
-cv::Mat grayWorldWhiteBalance(const cv::Mat& image) {
-    cv::Mat result;
     std::vector<cv::Mat> channels;
-    cv::split(image, channels);
-    std::vector<double> means;
-    for (int i = 0; i < 3; ++i) {
-        auto x = cv::mean(channels[i]);
-        means.push_back(x[0]);
-    }
-    for(int i = 0; i < 3; ++i)
-        channels[i] *= means[i]/cv::mean(means)[0];
+    cv::split(src, channels);
 
+    double avgB = cv::mean(channels[0])[0];
+    double avgG = cv::mean(channels[1])[0];
+    double avgR = cv::mean(channels[2])[0];
+
+    double avgGray = (avgB + avgG + avgR) / 3.0;
+
+    channels[0] = channels[0] * (avgGray / avgB);
+    channels[1] = channels[1] * (avgGray / avgG);
+    channels[2] = channels[2] * (avgGray / avgR);
+
+    cv::Mat result;
     cv::merge(channels, result);
+
     return result;
 }
 
-cv::Vec3f getColorInLinRGB(const cv::Mat& image, const cv::Point& point) {
-    cv::Vec3b sRGB_color = image.at<cv::Vec3b>(point);
-    cv::Vec3f linRGB_color;
-
-    for (int i = 0; i < 3; i++) {
-        float c = sRGB_color[i] / 255.0f;
-
-        if (c <= 0.04045) {
-            linRGB_color[i] = c / 12.92f;
-        } else {
-            linRGB_color[i] = std::pow((c + 0.055f) / 1.055f, 2.4f);
-        }
+// Функция для ручного автоконтрастирования
+cv::Mat autoContrast(const cv::Mat& src) {
+    if (src.empty()) {
+        std::cerr << "Error: Input image is empty!" << std::endl;
+        return cv::Mat();
     }
 
-    return linRGB_color;
+    cv::Mat result;
+    cv::normalize(src, result, 0, 255, cv::NORM_MINMAX);
+    return result;
 }
 
-void coutMetrics(const cv::Mat& image1, const cv::Mat& image2, const std::vector<Data>& data, std::string name) {
-    float metric1 = 0.0f;
-    float metric2 = 0.0f;
-    for (const auto& d : data) {
-        cv::Vec3f color1 = getColorInLinRGB(image1, d.point);
-        cv::Vec3f color2 = getColorInLinRGB(image2, d.point);
-        float norm = cv::norm(color1 - color2);
-        metric1 += norm;
-        metric2 += norm * norm;
-    }
+// Функция для вычисления SSIM
+cv::Mat computeSSIM(const cv::Mat& img1, const cv::Mat& img2, double& ssim_value) {
+    cv::Mat img1_gray, img2_gray;
+    cv::cvtColor(img1, img1_gray, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(img2, img2_gray, cv::COLOR_BGR2GRAY);
 
-    metric1 /= data.size();
-    metric2 /= data.size();
+    const double C1 = 6.5025, C2 = 58.5225;
 
-    std::cout << name << std::endl;
-    std::cout << "Mean: " << metric1 << std::endl;
-    std::cout << "RMS: " << std::sqrt(metric2) << std::endl;
+    cv::Mat I1, I2;
+    img1_gray.convertTo(I1, CV_32F);
+    img2_gray.convertTo(I2, CV_32F);
+
+    cv::Mat I1_2 = I1.mul(I1);
+    cv::Mat I2_2 = I2.mul(I2);
+    cv::Mat I1_I2 = I1.mul(I2);
+
+    cv::Mat mu1, mu2;
+    cv::GaussianBlur(I1, mu1, cv::Size(11, 11), 1.5);
+    cv::GaussianBlur(I2, mu2, cv::Size(11, 11), 1.5);
+
+    cv::Mat mu1_2 = mu1.mul(mu1);
+    cv::Mat mu2_2 = mu2.mul(mu2);
+    cv::Mat mu1_mu2 = mu1.mul(mu2);
+
+    cv::Mat sigma1_2, sigma2_2, sigma12;
+    cv::GaussianBlur(I1_2, sigma1_2, cv::Size(11, 11), 1.5);
+    cv::GaussianBlur(I2_2, sigma2_2, cv::Size(11, 11), 1.5);
+    cv::GaussianBlur(I1_I2, sigma12, cv::Size(11, 11), 1.5);
+
+    sigma1_2 -= mu1_2;
+    sigma2_2 -= mu2_2;
+    sigma12 -= mu1_mu2;
+
+    cv::Mat t1, t2, t3;
+    t1 = 2 * mu1_mu2 + C1;
+    t2 = 2 * sigma12 + C2;
+    t3 = t1.mul(t2);
+
+    t1 = mu1_2 + mu2_2 + C1;
+    t2 = sigma1_2 + sigma2_2 + C2;
+    t1 = t1.mul(t2);
+
+    cv::Mat ssim_map;
+    cv::divide(t3, t1, ssim_map);
+    ssim_value = cv::mean(ssim_map)[0];
+
+    return ssim_map;
 }
 
+// Функция для создания изображения с результатами SSIM
+cv::Mat createResultImage(double ssimGrayWorld, double ssimAutoContrast) {
+    cv::Mat resultImage = cv::Mat::zeros(200, 500, CV_8UC3);
+    cv::putText(resultImage, "SSIM Results", cv::Point(20, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255, 255, 255), 2);
+    cv::putText(resultImage, "Original vs Gray World: " + std::to_string(ssimGrayWorld), cv::Point(20, 80), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255), 2);
+    cv::putText(resultImage, "Original vs Auto Contrast: " + std::to_string(ssimAutoContrast), cv::Point(20, 130), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255), 2);
+
+    return resultImage;
+}
 
 int main() {
-    cv::Mat image = cv::imread("/home/art4m/misis/misis2024s-21-02-eroshkin-a-t/prj.lab/lab09/src/pic.png");
-    std::vector<Data> data = readDataFromJson("/home/art4m/misis/misis2024s-21-02-eroshkin-a-t/prj.lab/lab09/data.json");
-    cv::Mat lin_image = sRGB_to_linRGB(image);
-    cv::Mat white_balanced_image = grayWorldWhiteBalance(image);
-    cv::Mat contrasted_image = autoContrastColor(image, 0.1, 0.9);
+    std::string input = "C:/Users/79194/OneDrive - НИТУ МИСиС/Рисунки/Рандом фотки/Собака.jpg";
+    std::string outputGrayWorld = "C:/Users/79194/OneDrive - НИТУ МИСиС/Рисунки/Рандом фотки/Собака_grayworld.jpg";
+    std::string outputAutoContrast = "C:/Users/79194/OneDrive - НИТУ МИСиС/Рисунки/Рандом фотки/Собака_autocontrast.jpg";
 
-    coutMetrics(image, white_balanced_image, data, "White balance");
-    coutMetrics(image, contrasted_image, data, "Contrast");
+    // Загружаем изображение
+    cv::Mat img = cv::imread(input);
 
-    cv::imshow("Original", image);
-    cv::imshow("White Balanced", white_balanced_image);
-    cv::imwrite("White.png", white_balanced_image);
-    cv::imshow("Contrasted", contrasted_image);
-    cv::imwrite("Contrasted.jpg", contrasted_image);
+    cv::resize(img, img, cv::Size(500, 500));
+
+    // Применяем функцию Gray World
+    cv::Mat resultGrayWorld = grayWorld(img);
+
+    // Сохраняем результат Gray World
+    cv::imwrite(outputGrayWorld, resultGrayWorld);
+
+    // Применяем функцию автоконтрастирования
+    cv::Mat resultAutoContrast = autoContrast(img);
+    
+
+    // Сохраняем результат автоконтрастирования
+    cv::imwrite(outputAutoContrast, resultAutoContrast);
+
+    // Вычисляем SSIM между оригиналом и изображениями
+    double ssimGrayWorld;
+    double ssimAutoContrast;
+
+    cv::Mat ssimMapGrayWorld = computeSSIM(img, resultGrayWorld, ssimGrayWorld);
+    cv::Mat ssimMapAutoContrast = computeSSIM(img, resultAutoContrast, ssimAutoContrast);
+
+
+    // Создаем изображение с результатами SSIM
+    cv::Mat resultImage = createResultImage(ssimGrayWorld, ssimAutoContrast);
+
+    // Отображаем результат
+    cv::imshow("Original", img);
+    cv::imshow("Gray World", resultGrayWorld);
+    cv::imshow("Auto Contrast", resultAutoContrast);
+    cv::imshow("SSIM Results", resultImage);
+    cv::imwrite("SSIMdiff.png", resultImage);
+
     cv::waitKey(0);
 
     return 0;
